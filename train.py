b4790aab74e5a2d04f0016d2fe4321caab5d7d71
madonnaprayer <adriaanmenschikow@gmail.com>
2025-12-09 06:49:05 +0100

    feat: placed all configuration data to a file - started to write the training script

--- train.py
+++ train.py
@@ -1,17 +1,23 @@
 import os
 import time
 import math
-import numpy
-from config import *
+import numpy as np
 from model import FemtoGPT
+import config
 
+# use the device from config
+device = config.device
+
+
+# -----------------------------------------------------------------------------
 # configuration
+# -----------------------------------------------------------------------------
+
 # I/O
-out_dir = "out"
+os.makedirs(config.out_dir, exist_ok=True)
 eval_interval = 200  # how often to check validation loss
 log_interval = 10  # how often to print training stats
 eval_iters = 200  # how many batches to use for estimating loss
-os.makedirs(out_dir, exist_ok=True)
 
 # data
 batch_size = 32  # how many independent sequences to process in parallel
